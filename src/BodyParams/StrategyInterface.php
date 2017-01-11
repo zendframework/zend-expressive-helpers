5def95a2ceb80981c0b1fb194b7e65b4997f554b
Matthew Weier O'Phinney <matthew@zend.com>
2017-01-11 09:51:39 -0600

    Added license check to QA tools
    - Using malukenho/docheader
    - Updated composer scripts:
      - Added `license-check`, and added it to the `check` script
      - Renamed `coveralls` to `upload-coverage`
    - Updated travis configuration
      - Invoke `license-check` when CS checks are requested
      - Updated coveralls invocation to use `upload-coverage`
    - Fixed license headers across the repository so that they all now pass.

--- src/BodyParams/StrategyInterface.php
+++ src/BodyParams/StrategyInterface.php
@@ -1,7 +1,7 @@
 <?php
 /**
- * @see       http://github.com/zendframework/zend-expressive-helpers for the canonical source repository
- * @copyright Copyright (c) 2015 Zend Technologies USA Inc. (http://www.zend.com)
+ * @see       https://github.com/zendframework/zend-expressive-helpers for the canonical source repository
+ * @copyright Copyright (c) 2015-2017 Zend Technologies USA Inc. (http://www.zend.com)
  * @license   https://github.com/zendframework/zend-expressive-helpers/blob/master/LICENSE.md New BSD License
  */
 
