330e9f1b781dc1cfb74e1de52001b23df40c3cd9
Matthew Weier O'Phinney <matthew@zend.com>
2017-09-11 11:27:31 -0500

    Adds a ContentLengthMiddleware for auto-injecting the Content-Length response header
    This feature is pulled from zend-diactoros'
    `SapiEmitterTrait::injectContentLength() method; a future version will
    omit that functionality, in favor of using this new middleware.
    
    This middleware will inject a `Content-Length` response header based on
    the body size if:
    
    - no `Content-Length` header is already present AND
    - the body size is non-null
    
    Otherwise, it returns the response verbatim.

---
+++ test/ContentLengthMiddlewareTest.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * @see       https://github.com/zendframework/zend-expressive-helpers for the canonical source repository
+ * @copyright Copyright (c) 2017 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   https://github.com/zendframework/zend-expressive-helpers/blob/master/LICENSE.md New BSD License
+ */
+
+namespace ZendTest\Expressive\Helper;
+
+use Interop\Http\ServerMiddleware\DelegateInterface;
+use PHPUnit\Framework\TestCase;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Message\StreamInterface;
+use Zend\Expressive\Helper\ContentLengthMiddleware;
+
+class ContentLengthMiddlewareTest extends TestCase
+{
+    public function setUp()
+    {
+        $this->response = $response = $this->prophesize(ResponseInterface::class);
+        $this->request = $request = $this->prophesize(ServerRequestInterface::class)->reveal();
+        $this->stream = $this->prophesize(StreamInterface::class);
+
+        $delegate = $this->prophesize(DelegateInterface::class);
+        $delegate->process($request)->will([$response, 'reveal']);
+        $this->delegate = $delegate->reveal();
+
+        $this->middleware = new ContentLengthMiddleware();
+    }
+
+    public function testReturnsResponseVerbatimIfContentLengthHeaderPresent()
+    {
+        $this->response->hasHeader('Content-Length')->willReturn(true);
+        $response = $this->middleware->process($this->request, $this->delegate);
+        $this->assertSame($this->response->reveal(), $response);
+    }
+
+    public function testReturnsResponseVerbatimIfContentLengthHeaderNotPresentAndBodySizeIsNull()
+    {
+        $this->stream->getSize()->willReturn(null);
+        $this->response->hasHeader('Content-Length')->willReturn(false);
+        $this->response->getBody()->will([$this->stream, 'reveal']);
+
+        $response = $this->middleware->process($this->request, $this->delegate);
+        $this->assertSame($this->response->reveal(), $response);
+    }
+
+    public function testReturnsResponseWithContentLengthHeaderBasedOnBodySize()
+    {
+        $this->stream->getSize()->willReturn(42);
+        $this->response->hasHeader('Content-Length')->willReturn(false);
+        $this->response->getBody()->will([$this->stream, 'reveal']);
+        $this->response->withHeader('Content-Length', '42')->will([$this->response, 'reveal']);
+
+        $response = $this->middleware->process($this->request, $this->delegate);
+        $this->assertSame($this->response->reveal(), $response);
+    }
+}
